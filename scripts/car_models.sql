7e0d0e52ca5cadf802f9aca57c9f2dba5e86094e
Seolhee Kim <seolhee@upspringmedia.com>
2017-07-22 19:57:13 -1000

    added database indexing

--- scripts/car_models.sql
+++ scripts/car_models.sql
@@ -1,4 +1,9 @@
 -- db indexed_cars
+CREATE INDEX index_one ON car_models(make_title) WHERE make_code LIKE '%LAM%';
+CREATE INDEX index_two ON car_models(make_title) WHERE make_code LIKE '%NISSAN%' AND model_code LIKE '%GT-R%';
+CREATE INDEX index_three ON car_models(make_title) WHERE make_code LIKE '%LAM%';
+CREATE INDEX index_four ON car_models(make_title) WHERE year BETWEEN 2010 AND 2015;
+CREATE INDEX index_five ON car_models(make_title) WHERE year = 2010;
 
 CREATE TABLE IF NOT EXISTS car_models
 (
